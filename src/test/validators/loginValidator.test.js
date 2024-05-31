279d6dd21b43f18371c7ee1ad6248d7dfa6afe35
Aayushi Shah <aayu.shah711@gmail.com>
2024-05-31 18:13:19 +0530

    Test for login validator

---
+++ src/test/validators/loginValidator.test.js
@@ -0,0 +1,32 @@
+const loginValidator = require("../../validators/loginValidator");
+const { expect } = require("chai");
+
+describe("Login Validator", () => {
+  it("should validate a correct login", () => {
+    const user = { email: "testuser", password: "password123" };
+    const { error } = loginValidator.validate(user);
+    expect(error).to.be.undefined;
+  });
+
+  it("should return error for empty email", () => {
+    const user = { email: "", password: "password123" };
+    const { error } = loginValidator.validate(user);
+    expect(error).to.exist;
+    if (error) {
+      expect(error.details[0].message).to.equal(
+        '"email" is not allowed to be empty'
+      );
+    }
+  });
+
+  it("should return error for empty password", () => {
+    const user = { email: "test@example.com", password: "" };
+    const { error } = loginValidator.validate(user);
+    expect(error).to.exist;
+    if (error) {
+      expect(error.details[0].message).to.equal(
+        '"password" is not allowed to be empty'
+      );
+    }
+  });
+});
