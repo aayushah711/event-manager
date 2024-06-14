234da75f24f7339315f0c0f83500801ef0f30c1e
Aayushi Shah <aayu.shah711@gmail.com>
2024-06-14 14:20:24 +0530

    Fix failing tests

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,7 +11,7 @@ function authenticateToken(req, res, next) {
   if (process.env.API_SECRET) {
     jwt.verify(token, process.env.API_SECRET, (err, user) => {
       if (err)
-        return res.status(403).json({ error: "Invalid or expired token" });
+        return res.status(401).json({ error: "Invalid or expired token" });
       req.user = user;
       next();
     });
--- src/test/controllers/eventController.test.js
+++ src/test/controllers/eventController.test.js
@@ -9,13 +9,22 @@ const { expect } = chai;
 
 describe("Event Controller", () => {
   let request;
-  const user = { email: "test@example.com", password: "password123" };
+  const email = "test@example.com";
+  const password = "password123";
+
   let accessToken;
   before(async () => {
     try {
       request = chai.request(app).keepOpen();
-      await request.post("/auth/register").send(user);
-      const res = await request.post("/auth/login").send(user);
+      await request.post("/auth/register").send({
+        email,
+        password,
+        role: "organizer",
+      });
+      const res = await request.post("/auth/login").send({
+        email,
+        password,
+      });
       accessToken = res.body.token;
     } catch (error) {
       console.error("Error registering user", error);
@@ -40,16 +49,15 @@ describe("Event Controller", () => {
   it("should create a new event", async () => {
     try {
       const event = {
-        name: "Test Event",
-        date: new Date(),
+        title: "Test Event",
+        date: new Date().toISOString(),
         time: new Date().toLocaleTimeString(),
         description: "Test description",
       };
-      const res = await request
+      const res = await chai
+        .request(app)
         .post("/events")
-        .set("x-auth-token", accessToken)
-        .set("username", "password")
-        .set("content-type", "application/json")
+        .set("authorization", `Bearer ${accessToken}`)
         .send(event);
       expect(res).to.have.status(201);
       expect(res.body).to.include(event);
@@ -62,7 +70,9 @@ describe("Event Controller", () => {
 
 // Mocking with Sinon
 describe("Event Controller with Sinon", () => {
-  let findStub, createStub;
+  let findStub, createStub, authenticateTokenStub;
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NmMwMDMyMTlkNzkwYmZhZjgwOTI2MyIsImVtYWlsIjoidGVzdEBleGFtcGxlLmNvbSIsInJvbGUiOiJvcmdhbml6ZXIiLCJpYXQiOjE3MTgzNTM5NzAsImV4cCI6MTcxODM1NzU3MH0.gmtj9FgiMGgE8LE-rKQ57MFjVHu24Dhny5lSywQgYSU";
 
   beforeEach(() => {
     findStub = sinon.stub(Event, "find");
@@ -76,10 +86,10 @@ describe("Event Controller with Sinon", () => {
 
   it("should get all events with sinon stub", async () => {
     try {
-      findStub.resolves([{ name: "Mock Event" }]);
+      findStub.resolves([{ title: "Mock Event" }]);
       const res = await chai.request(app).get("/events");
       expect(res).to.have.status(200);
-      expect(res.body).to.deep.equal([{ name: "Mock Event" }]);
+      expect(res.body).to.deep.equal([{ title: "Mock Event" }]);
     } catch (error) {
       console.error("Error fetching events with sinon stub", error);
       throw error;
@@ -88,9 +98,18 @@ describe("Event Controller with Sinon", () => {
 
   it("should create a new event with sinon stub", async () => {
     try {
-      const event = { name: "Mock Event", date: "2024-05-30" };
+      const event = {
+        title: "Test Event",
+        date: new Date().toISOString(),
+        time: new Date().toLocaleTimeString(),
+        description: "Test description",
+      };
       createStub.resolves(event);
-      const res = await chai.request(app).post("/events").send(event);
+      const res = await chai
+        .request(app)
+        .post("/events")
+        .set("authorization", `Bearer ${accessToken}`)
+        .send(event);
       expect(res).to.have.status(201);
       expect(res.body).to.include(event);
     } catch (error) {
